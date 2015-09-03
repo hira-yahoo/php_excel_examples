8ca251c8804a1c75d0dffab376c095e487d403f2
hira-yahoo <misocasu@yahoo.co.jp>
2015-09-03 14:59:55 +0900

    revise a sample program.

--- using_template.php
+++ using_template.php
@@ -3,13 +3,15 @@ include_once ( __DIR__ . '/PHPExcel_1.8.0_doc/Classes/PHPExcel.php');
 include_once ( __DIR__ . '/PHPExcel_1.8.0_doc/Classes/PHPExcel/IOFactory.php');
 
 $reader = PHPExcel_IOFactory::createReader('Excel2007');
-$excel = $reader->load("template.xlsx");
+// $excel = $reader->load("template.xlsx");
+$excel = $reader->load("mitsumori.xlsx");
+
 
 // Excel2007形式(xlsx)で出力する
 $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
 
 $sheet = $excel->getActiveSheet();//有効になっているシートを代入
-$sheet->setCellValue('A2', isset($_POST['name']) ? $_POST['name'].'さん' : '名無しさん'); //A2のセルに名前を入力
+$sheet->setCellValue('A6', isset($_POST['name']) ? $_POST['name'].'さん' : '名無しさん'); // セルに名前を入力
 
 $file_name = "template_output.xlsx"; //ダウンロードさせるファイル名
 
